b15ee49c70ed3b4efd704044e29856fb4e64524e
dr0psh0t <xxrequiemofsoulsxx@gmail.com>
2022-02-16 10:27:55 +0800

    autocloseable

---
+++ com/effectivejava/autocloseabletest/ResourceSample.java
@@ -0,0 +1,13 @@
+package com.effectivejava.autocloseabletest;
+
+public class ResourceSample implements AutoCloseable {
+
+    public void doWork() {
+        System.out.println("Doing Work");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Calling close()");
+    }
+}
---
+++ com/effectivejava/autocloseabletest/ResourceTest.java
@@ -0,0 +1,12 @@
+package com.effectivejava.autocloseabletest;
+
+public class ResourceTest {
+
+    public static void main(String[] args) {
+        try (ResourceSample resourceSample = new ResourceSample()) {
+            resourceSample.doWork();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+}
